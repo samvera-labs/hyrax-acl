e44bfb00f9b9f5f9bcce4c84e281e7e3212d34f9
Longshou Situ <lsitu@ucsd.edu>
2022-07-08 15:14:59 -0700

    Add ACL group read interface - By @tchu and @lsitu.

--- lib/hyrax/acl.rb
+++ lib/hyrax/acl.rb
@@ -2,6 +2,7 @@ require 'valkyrie'
 require 'hyrax/acl/permission'
 require 'hyrax/acl/access_control'
 require 'hyrax/acl/custom_queries'
+require 'hyrax/acl/access_control_list'
 
 ##
 # Hyrax Access Control Lists
